480ffecaefbc4452d99168c25610a8c06edf4b0c
Ludovic HENRY <ludovic.henry@me.com>
2021-01-17 10:18:43 -0400

    Initial Commit

---
+++ NiceSpot/NiceSpotApp.swift
@@ -0,0 +1,20 @@
+//
+//  NiceSpotApp.swift
+//  NiceSpot
+//
+//  Created by Ludovic HENRY on 17/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct NiceSpotApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
