73337842b4ec4ca8cceab0dcbaa7d35794da6867
Ludovic HENRY <ludovic.henry@me.com>
2021-01-19 20:07:28 -0400

    Removed unused files

---
+++ NiceSpot/View/SpotDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  SpotDetailView.swift
+//  NiceSpot
+//
+//  Created by Ludovic HENRY on 19/01/2021.
+//
+
+import SwiftUI
+
+struct SpotDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpotDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpotDetailView()
+    }
+}
