63e97c6dd28e8e7474b9f6d171e2737887a2dbc5
Ludovic HENRY <ludovic.henry@me.com>
2021-01-25 21:24:01 -0400

    Added a personalized back button

--- NiceSpot/View/SpotDetailView.swift
+++ NiceSpot/View/SpotDetailView.swift
@@ -12,81 +12,100 @@ struct SpotDetailView: View {
     @ObservedObject var content: SpotDetailContent
 
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            NavigationLink(
-                destination: content.image
-                    .navigationTitle(content.title),
-                label: {
-                    content.image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                })
+        ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
             Rectangle()
-                .foregroundColor(.white)
-                .frame(height: 50)
-                .cornerRadius(30)
-                .offset(y: -30)
-                .padding(.bottom, -50)
-            VStack {
-                HStack {
-                    Text(content.title)
-                        .font(.title)
-                        .fontWeight(.regular)
-                        .lineLimit(2)
-                    Spacer()
-                }
-                HStack {
-                    Image("\(content.category)")
-                        .resizable()
-                        .frame(width: 24, height: 24)
-                        .clipShape(Capsule())
-                    Text(content.category)
-                        .font(.caption)
-                        .fontWeight(.black)
-                        .padding(5)
-                        .background(Color.green)
-                        .clipShape(Capsule())
-                        .foregroundColor(.white)
-                    Text(content.municipality)
-                        .font(.caption)
-                        .fontWeight(.black)
-                        .padding(5)
-                        .background(Color.orange)
-                        .clipShape(Capsule())
-                        .foregroundColor(.white)
-                    Spacer()
-                }
-            }
-            .padding()
-            Divider()
-            Text(content.detail)
-                .font(.body)
-                .foregroundColor(Color.gray)
-                .padding()
-            NavigationLink(
-                destination:
-                    VStack {
-                        MapView(spotLocation: content.location)
-                        Link("Open in Maps", destination: content.mapLink)
+                .foregroundColor(.clear)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            ScrollView(.vertical, showsIndicators: false) {
+                NavigationLink(
+                    destination: content.image
+                        .navigationTitle(content.title),
+                    label: {
+                        content.image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    })
+                Rectangle()
+                    .foregroundColor(.white)
+                    .frame(height: 50)
+                    .cornerRadius(30)
+                    .offset(y: -30)
+                    .padding(.bottom, -50)
+                VStack {
+                    HStack {
+                        Text(content.title)
+                            .font(.title)
+                            .fontWeight(.regular)
+                            .lineLimit(2)
+                        Spacer()
                     }
-                    .navigationTitle(content.title)
-                ,
-                label: {
-                    MapView(spotLocation: content.location)
-                        .frame(height: 300)
-                        .cornerRadius(10)
-                })
+                    HStack {
+                        Image("\(content.category)")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .clipShape(Capsule())
+                        Text(content.category)
+                            .font(.caption)
+                            .fontWeight(.black)
+                            .padding(5)
+                            .background(Color.green)
+                            .clipShape(Capsule())
+                            .foregroundColor(.white)
+                        Text(content.municipality)
+                            .font(.caption)
+                            .fontWeight(.black)
+                            .padding(5)
+                            .background(Color.orange)
+                            .clipShape(Capsule())
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                }
+                .padding()
+                Divider()
+                Text(content.detail)
+                    .font(.body)
+                    .foregroundColor(Color.gray)
+                    .padding()
+                NavigationLink(
+                    destination:
+                        VStack {
+                            MapView(spotLocation: content.location)
+                            Link("Open in Maps", destination: content.mapLink)
+                        }
+                        .navigationTitle(content.title)
+                    ,
+                    label: {
+                        MapView(spotLocation: content.location)
+                            .frame(height: 300)
+                            .cornerRadius(10)
+                    })
+            }
+            BackButton()
+                .padding([.top, .leading], 5.0)
 
+            
         }
         .onAppear{ content.loadImage()}
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            ToolbarItem(placement: .principal ) {
-                Text(content.category)
-            }
-        }
+        .navigationBarHidden(true)
     }
 }
+
+struct BackButton: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    var body: some View {
+        Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }, label: {
+            Image(systemName: "arrow.backward.circle.fill")
+                .resizable()
+                .frame(width: 35, height: 35)
+                .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.3))
+            
+        })
+    }
+}
+
 
 struct SpotDetailView_Previews: PreviewProvider {
     static var previews: some View {
