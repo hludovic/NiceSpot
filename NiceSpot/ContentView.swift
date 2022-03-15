d586d9f88e335a19c15688701888d4c5d4eec052
Ludovic HENRY <badsteam@hotmail.com>
2022-03-15 09:20:27 -0400

    Initial Commit

---
+++ NiceSpot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NiceSpot
+//
+//  Created by Ludovic HENRY on 15/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NiceSpot/NiceSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceSpotApp.swift
+//  NiceSpot
+//
+//  Created by Ludovic HENRY on 15/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NiceSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
